a88995b65fa6af74ecd039f872725a6ad4f49c82
orvice <git@orx.me>
2017-05-21 02:01:54 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# from https://github.com/fr3nd/docker-supervisor/blob/master/supervisord.conf
-
 set -e
-
-# ln -s /etc/supervisor/supervisord.conf /etc/supervisord.conf
-
-if [ -z "$@" ]; then
-  exec /usr/local/bin/supervisord -c /etc/supervisord.conf --nodaemon
-else
-  exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
-fi
